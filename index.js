204da042999a83594bdf6dccd575c3b9652e373e
TapanBala <tapanbala007@gmail.com>
2020-05-19 00:22:09 +0530

    Barebone implementation of tracer.js with timeout example

---
+++ index.js
@@ -0,0 +1,19 @@
+// An initial example to check if tracer only traces the required async function.
+
+import Tracer from "./src/tracer.js";
+
+function doTimeout() {
+    console.log("Starting timeout.");
+
+    setTimeout(() => {
+        console.log("Timeout finished.");
+    }, 2000);
+}
+
+const tracer = new Tracer();
+
+tracer.inject('test-1', doTimeout);
+console.log("Middle of script");
+tracer.stop();
+
+console.log("End of script");
---
+++ src/tracer.js
@@ -0,0 +1,150 @@
+import async_hooks from "async_hooks";
+import fs from "fs";
+import util from "util";
+
+class Tracer {
+
+    // Tracks async operations currently in progress.
+    //
+    curAsyncOps = new Map();
+
+    // Async operations at the root of the hierarchy.
+    //
+    rootAsyncOps = new Map();
+
+    // Records all async operations that have ever happened.
+    //
+    allAsyncOps = new Map();
+
+    // Records the number of async operations in progress for each label.
+    //
+    numAsyncOps = new Map();
+
+    // Records the top-level execution contexts currently being tracked.
+    //
+    executionContexts = new Set();
+
+    // Maps execution context ids to labels.
+    //
+    labelMap = new Map();
+
+    inject = (label, fn) => {
+        this.init();
+
+        const executionContext = new async_hooks.AsyncResource(label)
+        executionContext.runInAsyncScope(() => {
+            const executionContextAsyncId = async_hooks.executionAsyncId();
+            this.executionContexts.add(executionContextAsyncId);
+            this.labelMap.set(executionContextAsyncId, label);
+            fn();
+        });
+    }
+
+    stop = () => {
+        if (this.asyncHook) {
+            // checkAsyncOps(this, () => this.asyncHook.disable())
+            this.asyncHook.disable()
+        }
+    }
+
+    init = () => {
+        if (!this.asyncHook) {
+            this.asyncHook = async_hooks.createHook({
+                init: (asyncId, type, triggerAsyncId, resource) => {
+                    this.addAsyncOp(asyncId, type, triggerAsyncId, resource);
+                },
+                before: (asyncId) => {
+                    this.debug("before executing asyncId: ", asyncId);
+                },
+                after: (asyncId) => {
+                    this.debug("after successful callback of asyncId: ", asyncId);
+                },
+                destroy: (asyncId) => {
+                    this.removeAsyncOp(asyncId, "destroying asyncId: ");
+                },
+                promiseResolve: (asyncId) => {
+                    this.removeAsyncOp(asyncId, "promiseResolve asyncId: ");
+                }
+            })
+        }
+
+        this.asyncHook.enable();
+    }
+
+    findExecutionContextId = (asyncId) => {
+        if (this.executionContexts.has(asyncId)) {
+            return asyncId;
+        }
+
+        const asyncOp = this.allAsyncOps.get(asyncId);
+        if (asyncOp) {
+            return asyncOp.executionContextId;
+        }
+
+        return undefined;
+    }
+
+    addAsyncOp = (asyncId, type, triggerAsyncId, resource) => {
+        const executionContextId = this.findExecutionContextId(triggerAsyncId);
+        if (executionContextId === undefined) {
+            return;
+        }
+
+        const error = {};
+        Error.captureStackTrace(error);
+
+        const stack = error.stack.split("\n").map(line => line.trim());
+
+        const asyncOp = {
+            asyncId,
+            type,
+            triggerAsyncId,
+            children: new Map(),
+            stack,
+            status: "in-flight",
+            executionContextId,
+        };
+
+        const parentOperation = this.allAsyncOps.get(triggerAsyncId);
+        if (parentOperation) {
+            parentOperation.children.set(asyncId, asyncOp); // Record the hierarchy of asynchronous operations.
+        } else {
+            this.rootAsyncOps.set(asyncId, asyncOp);
+        }
+
+        this.curAsyncOps.set(asyncId, asyncOp);
+        this.allAsyncOps.set(asyncId, asyncOp);
+
+        this.numAsyncOps.set(executionContextId, (this.numAsyncOps.get(executionContextId) || 0) + 1);
+
+        this.debug("initialized type: ", type, "with asyncId: ", asyncId, "having triggerAsyncId: ", triggerAsyncId);
+    }
+
+    removeAsyncOp = (asyncId, reason) => {
+        const asyncOp = this.curAsyncOps.get(asyncId)
+        if (!asyncOp) {
+            return;
+        }
+
+        asyncOp.status = "completed";
+
+        this.curAsyncOps.delete(asyncId);
+
+        const executionContextId = this.findExecutionContextId(asyncId);
+        if (executionContextId !== undefined) {
+            const numAsyncOps = this.numAsyncOps.get(executionContextId);
+            if (numAsyncOps !== undefined) {
+                this.numAsyncOps.set(executionContextId, numAsyncOps - 1);
+                this.debug(reason, asyncId);
+            }
+        }
+    }
+
+    debug = (...args) => {
+        fs.writeFileSync(1, `${util.format(...args)}\n`, {flag: 'a'});
+    }
+}
+
+export default Tracer;
+//
+// module.exports = Tracer;
